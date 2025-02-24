8e0b8168c599e854d4a2dabc20a9d3036c202135
karij5 <karij5@farmingdale.edu>
2025-02-24 18:38:37 -0500

    added UserDetails

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express=require("express");
 const app=express();
+const mongoose= require("mongoose");
+
+const mongoUrl="mongodb+srv://invo_user:invo_user_password@cluster0.47a10.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(mongoUrl).then(()=>{
+    console.log("Mongodb Connected");
+})
+.catch((e)=>{
+    console.log(e);
+})
 
 app.get("/" ,(req, res)=>{
     res.send({status: "Started"})
