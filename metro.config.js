40bd557e8935d66b6698d9b8c49eaab8533a2365
John Distler <94850886+ZonDizzler@users.noreply.github.com>
2025-03-06 09:54:29 -0500

    added comments to metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
+//Retrieve the default Metro bundler configuration
 const defaultConfig = getDefaultConfig(__dirname);
+
+//Adds the .cjs extension to the list of recognized asset extensions
 defaultConfig.resolver.assetExts.push('cjs');
 
+//Exports the modified configuration for metro to use
 module.exports = defaultConfig;
