6f2acc9a83149bd61d9d69886043a30db13b251f
John Distler <94850886+ZonDizzler@users.noreply.github.com>
2025-03-06 15:50:38 -0500

    display quarans in section list

--- app/(tabs)/items.tsx
+++ app/(tabs)/items.tsx
@@ -1,17 +1,84 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+  SectionList,
+} from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../FirebaseConfig";
 
 export default function Tab() {
+  const [data, setData] = useState<any[]>([]);
+
+  //Fetch data when the component initially mounts
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  //Fetch data from API
+  async function fetchData() {
+    try {
+      const querySnapshot = await getDocs(collection(db, "Qurans"));
+
+      const documents = querySnapshot.docs.map((doc) => ({
+        id: doc.id, // Store document ID
+        quantity: doc.data().Quantity,
+      }));
+
+      setData([
+        {
+          title: "Quarans",
+          data: documents,
+        },
+      ]);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Tab items</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <SectionList
+          sections={data}
+          renderItem={({ item }) => (
+            <View style={styles.item}>
+              <Text>
+                {item.id}: {item.quantity}
+              </Text>
+            </View>
+          )}
+          renderSectionHeader={({ section: { title } }) => (
+            <View style={styles.header}>
+              <Text>{title}</Text>
+            </View>
+          )}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 32,
   },
 });
